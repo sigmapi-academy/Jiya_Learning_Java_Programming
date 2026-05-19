261536b72daf269bf4bb53e577d6a49c49e5f2fd
sigmapi-academy <kumarsatyendermishra@gmail.com>
2026-05-19 21:49:03 +0530

    datatypes in Java

---
+++ Coding/Learning_Java/PrimitiveDataTypes.java
@@ -0,0 +1,31 @@
+
+/**
+ * Write a description of class PrimitiveDataTypes here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class PrimitiveDataTypes
+{
+    public static void main(String[] args){
+        boolean b = true;
+        byte bt = 10;
+        char c = '\u20b9';
+        short s = 3456;
+        int i = 3545678;
+        float f = 3.14f;
+        double d = 0.987654323456;
+        long l = 1234567897654345678L;
+        String str = "Hello!  Java";
+        
+        System.out.print("\fb = " + b +
+                        "\nbt = " + bt +
+                        "\nc = " + c +
+                        "\ns = " + s + 
+                        "\ni = " + i +
+                        "\nf = " + f +
+                        "\nd = " + d + 
+                        "\nl = " + l +
+                        "\nstr = " + str);
+    }
+}
